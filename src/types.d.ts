f392a4a7bd2f319a4755d9cf5d45c7e21e47670a
kvbalib <zero@fucks.gives>
2024-09-26 17:13:01 +0200

    refactor(module): restore default types

--- src/types.d.ts
+++
@@ -1,6 +0,0 @@
-declare module 'react-native-lecom-scan' {
-  import type { LecomHook, LecomToggleScan } from '../types'
-
-  export const useLecomScan: LecomHook
-  export const toggleScan: LecomToggleScan
-}
